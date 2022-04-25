4315b4a8524cab30989362de9abeb3f6fc504552
adnankaraduman <99875354+adnankaraduman@users.noreply.github.com>
2022-04-25 15:24:30 +0300

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,14 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        System.out.println("Hello Horld");
+        System.out.println("Hello Morld");
+        
+        
+        
+        
+        
+        
 
 
     }
